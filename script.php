1e9872acf57cbac9bf1a60564fae8d32ce729ef5
Denis <116643478+Der-Denis@users.noreply.github.com>
2023-07-07 12:37:03 +0200

    Dokumentation weiter
    Kostenkalkulation erstellt

--- script.php
+++ script.php
@@ -10,12 +10,14 @@
     {
         public $spieler1;
         public $spieler2;
+        public $token; # welcher Spieler ist dran.
         public $spielfeld;
 
-        function __construct($spieler1, $spieler2, $spielfeld)
+        function __construct($spieler1, $spieler2, $token, $spielfeld)
         {
             $this->spieler1 = $spieler1;
             $this->spieler2 = $spieler2;
+            $this->token = $token;
             $this->spielfeld = $spielfeld;
         }
     }
